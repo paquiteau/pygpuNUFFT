05cbe7f446c0acd6b85b291df878bb913a4d0b6c
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-01-13 11:05:45 +0100

    use arguments for density script

--- tests/test_density_comp.py
+++ tests/test_density_comp.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import argparse
+
 import numpy as np
 # gpuNUFFT import
 from mri.operators import NonCartesianFFT
@@ -7,33 +9,58 @@ from mri.operators.fourier.utils import estimate_density_compensation
 from pysap.data import get_sample_data
 
 
-img3d = get_sample_data("3d-pmri").data.astype(np.complex64)
-samples3d = get_sample_data("mri-radial-3d-samples").data
-samples3d *= np.pi / samples3d.max()
-
-# img2d = get_sample_data("2d-pmri").data.astype(np.complex64)
-# samples2d = get_sample_data("mri-radial-samples").data
-# samples2d *= 2 * np.pi
-# shape2d = (512, 512)
-# n_samples2d = 32768
-#  img2dssos = np.linalg.norm(img2d,axis=0).astype(np.complex64)
+parser = argparse.ArgumentParser(description='test density_compensation.')
+parser.add_argument('dim', metavar='dim', type=int,
+                    help='select dimension')
 
 
-smaps3d = np.ones_like(img3d, dtype='complex128')
-smaps3d = smaps3d * np.arange(1, len(smaps3d) + 1)[:, np.newaxis, np.newaxis, np.newaxis]
-n_coils = 32
-n_samples3d = 6136781
-shape3d = (128, 128, 160)
-img3dssos = np.linalg.norm(img3d,axis=0).astype(np.complex64)
-
-grid_op = NonCartesianFFT(
+def test_density3D():
+    print("# 3D DensityCompensation")
+    img3d = get_sample_data("3d-pmri").data.astype(np.complex64)
+    samples3d = get_sample_data("mri-radial-3d-samples").data
+    samples3d *= np.pi / samples3d.max()
+    n_coils = 32
+    n_samples3d = 6136781
+    shape3d = (128, 128, 160)
+    grid_op = NonCartesianFFT(
         samples=samples3d,
         shape=shape3d,
         implementation='gpuNUFFT',
         osf=1,
-)
+    )
 
-density_new = grid_op.impl.operator.estimate_density_comp(10)
+    density_new = grid_op.impl.operator.estimate_density_comp(10)
+    density_comp3d = estimate_density_compensation(samples3d, shape3d, 10)
+    print(np.allclose(density_comp3d, density_new))
 
-density_comp3d = estimate_density_compensation(samples3d, shape3d)
-print(np.allclose(density_comp3d, density_new))
+def test_density2D():
+    print("# 2D DensityCompensation")
+    img2d = get_sample_data("2d-pmri").data.astype(np.complex64)
+    samples2d = get_sample_data("mri-radial-samples").data
+    samples2d *= 2 * np.pi
+    shape2d = (512, 512)
+    n_samples2d = 32768
+    img2dssos = np.linalg.norm(img2d, axis=0).astype(np.complex64)
+
+    grid_op = NonCartesianFFT(
+        samples=samples2d,
+        shape=shape2d,
+        implementation='gpuNUFFT',
+        osf=1,
+    )
+
+    density_new = grid_op.impl.operator.estimate_density_comp(10)
+    density_comp2d = estimate_density_compensation(samples2d, shape2d, 10)
+    print(np.allclose(density_comp3d, density_new))
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    if args.dim == 3:
+        test_density3D()
+    elif args.dim == 2:
+        test_density2D()
+    else:
+        test_density2D()
+        test_density3D()
