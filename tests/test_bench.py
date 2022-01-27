5b3fee7dcfa76dc6262b11812cf452a83830223d
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-01-27 13:45:01 +0100

    add performance counter

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
-
+from time import perf_counter
 import numpy as np
 # gpuNUFFT import
 from mri.operators import NonCartesianFFT
@@ -9,11 +9,12 @@ from pysap.data import get_sample_data
 
 
 parser = argparse.ArgumentParser(description='test density_compensation.')
-parser.add_argument('dim', metavar='dim', type=int,
+parser.add_argument('dim', metavar='dim', type=int, const=0, default=0, nargs='?',
                     help='select dimension')
+parser.add_argument('--new', action='store_true', default=False)
+parser.add_argument('--gpu-density', dest='density', action='store_true', default=False)
 
-
-def test_gradient(image, shape, n_coils, samples, samples_data, smaps=None, density=None):
+def test_gradient(image, shape, n_coils, samples, samples_data, smaps=None, density=None, new=False):
     fourier_op = NonCartesianFFT(samples=samples,
                                  shape=shape,
                                  density_comp=density,
@@ -21,17 +22,25 @@ def test_gradient(image, shape, n_coils, samples, samples_data, smaps=None, dens
                                  smaps=smaps,
                                  implementation="gpuNUFFT",
                                  )
-    print("fourier_op init done")
+    ts = perf_counter()
     gradient = fourier_op.adj_op(fourier_op.op(image) - samples_data)
-    print("gradient classic done")
-    gradient_new = fourier_op.impl.operator.data_consistency(
-        image, samples_data)
-    print("gradient new done")
-    print(np.allclose(gradient, gradient_new.T))
+    tf = perf_counter()
+    print("elapsed_time: {:.3f}s".format(tf-ts))
+    print("||g||= ", np.linalg.norm(gradient.flatten()))
+    if new:
+        ts2 = perf_counter()
+        gradient_new = fourier_op.impl.operator.data_consistency(
+            np.reshape(image.T, image.size), samples_data)
+        tf2 = perf_counter()
+        print("elapsed_time: {:.3f}s, x{:.1f}".format(tf2-ts2, (tf-ts)/(tf2-ts2)))
+        print("||g_new||= ", np.linalg.norm(gradient_new.flatten()))
+        print("allclose: ", np.allclose(gradient, gradient_new.T))
+        print("allclose_reverse: ", np.allclose(gradient_new.T, gradient))
+        print("||g-g_new||/||g||= ", np.linalg.norm((gradient- gradient_new.T).flatten())/np.linalg.norm(gradient))
 
 
-def test_gradient3D():
-    print("# 3D data_consistency")
+def test_gradient3D(new, density):
+    print("# 3D gradient")
     img3d = get_sample_data("3d-pmri").data.astype(np.complex64)
     samples3d = get_sample_data("mri-radial-3d-samples").data
     samples3d *= np.pi / samples3d.max()
@@ -40,21 +49,24 @@ def test_gradient3D():
     smaps3d = np.ones_like(img3d, dtype='complex128')
     smaps3d = smaps3d * np.arange(1, len(smaps3d) + 1)[:, np.newaxis, np.newaxis, np.newaxis]
     fake_data3d = np.ones(len(samples3d), dtype='complex64')
-    grid_op = NonCartesianFFT(
-        samples=samples3d,
-        shape=shape3d,
-        implementation='gpuNUFFT',
-        osf=1,
-    )
+    img3dssos = np.linalg.norm(img3d, axis=0)
+    if density:
+        grid_op = NonCartesianFFT(
+                samples=samples3d,
+                shape=shape3d,
+                implementation="gpuNUFFT",
+                osf=1,
+            )
 
-    img3dssos = np.linalg.norm(img3d)
-    density = grid_op.impl.operator.estimate_density_comp(10)
-    del grid_op
+        density = grid_op.impl.operator.estimate_density_comp(10)
+        del grid_op
+    else:
+        density = np.ones(len(samples3d), dtype='float32')
 
-    return test_gradient(img3dssos, shape3d, n_coils, samples3d, fake_data3d, smaps=smaps3d, density=density)
+    return test_gradient(img3dssos, shape3d, n_coils, samples3d, fake_data3d, smaps=smaps3d, density=density, new=new)
 
 
-def test_gradient2D():
+def test_gradient2D(new,density):
     print("# 2D data_consistency")
     img2d = get_sample_data("2d-pmri").data.astype(np.complex64)
     samples2d = get_sample_data("mri-radial-samples").data
@@ -65,29 +77,35 @@ def test_gradient2D():
     smaps2d = np.ones_like(img2d, dtype='complex64')
     smaps2d = smaps2d * np.arange(1, len(smaps2d) + 1)[:, np.newaxis, np.newaxis]
     fake_data2d = np.ones(len(samples2d), dtype='complex64')
-    grid_op = NonCartesianFFT(
-        samples=samples2d,
-        shape=shape2d,
-        implementation='gpuNUFFT',
-        osf=1,
-    )
-    img2dssos = np.linalg.norm(img2d)
-    density = grid_op.impl.operator.estimate_density_comp(10)
-    del grid_op
+
+    img2dssos = np.linalg.norm(img2d,axis=0)
+    if density:
+        grid_op = NonCartesianFFT(
+                samples=samples2d,
+                shape=shape2d,
+                implementation="gpuNUFFT",
+                osf=1,
+            )
+
+        density = grid_op.impl.operator.estimate_density_comp(10)
+        del grid_op
+    else:
+        density = np.ones(len(samples2d), dtype='float32')
 
     return test_gradient(img2dssos, shape2d,
                          n_coils, samples2d,
                          fake_data2d, smaps=smaps2d,
-                         density=density)
+                         density=density, new=new)
 
 
 if __name__ == "__main__":
+
     args = parser.parse_args()
 
     if args.dim == 3:
-        test_gradient3D()
+        test_gradient3D(args.new, args.density)
     elif args.dim == 2:
-        test_gradient2D()
+        test_gradient2D(args.new, args.density)
     else:
-        test_gradient2D()
-        test_gradient3D()
+        test_gradient2D(args.new, args.density)
+        test_gradient3D(args.new, args.density)
--- tests/test_density_comp.py
+++ tests/test_density_comp.py
@@ -2,6 +2,7 @@
 
 import argparse
 
+from time import perf_counter
 import numpy as np
 # gpuNUFFT import
 from mri.operators import NonCartesianFFT
@@ -13,58 +14,57 @@ parser = argparse.ArgumentParser(description='test density_compensation.')
 parser.add_argument('dim', metavar='dim', type=int, const=0,
                     default=0, nargs='?',
                     help='select dimension')
+parser.add_argument('--iter', type=int, default=10, help="number of iteration")
+parser.add_argument('--new', action='store_true', default=False, )
 
 
-def test_density3D():
+
+
+def test_density(samples, shape, new, itr=10):
+
+    ts = perf_counter()
+    density_comp = estimate_density_compensation(samples, shape, itr)
+    tf = perf_counter()
+    print("elapsed_time: {:.3f}s".format(tf-ts))
+    print("||d||= ", np.linalg.norm(density_comp))
+    if new:
+        ts2 = perf_counter()
+        grid_op = NonCartesianFFT(
+                samples=samples,
+                shape=shape,
+                implementation="gpuNUFFT",
+                osf=1,
+            )
+        density_new = grid_op.impl.operator.estimate_density_comp(itr)
+        tf2 = perf_counter()
+        print("elapsed_time: {:.3f}s, x{:.1f}".format(tf2-ts2, (tf-ts)/(tf2-ts2)))
+        print("||d_new||= ", np.linalg.norm(density_new))
+        print("allclose: ", np.allclose(density_comp, density_new))
+        print("||d-d_new||/||d|| = ",np.linalg.norm(density_comp - density_new) / np.linalg.norm(density_comp))
+
+def test_density3D(new, itr=10):
     print("# 3D DensityCompensation")
-    img3d = get_sample_data("3d-pmri").data.astype(np.complex64)
     samples3d = get_sample_data("mri-radial-3d-samples").data
-    samples3d *= np.pi / samples3d.max()
     shape3d = (128, 128, 160)
-    grid_op = NonCartesianFFT(
-        samples=samples3d,
-        shape=shape3d,
-        implementation='gpuNUFFT',
-        osf=1,
-    )
-
     print("samples: ", samples3d.shape)
-    print("img size ", img3d.shape)
-    density_new = grid_op.impl.operator.estimate_density_comp(10)
-    density_comp3d = estimate_density_compensation(samples3d, shape3d, 10)
-    print(np.allclose(density_comp3d, density_new))
-    print(np.linalg.norm(density_comp3d - density_new) / np.linalg.norm(density_comp3d))
+    print("img size ", shape3d)
+    return test_density(samples3d, shape3d, new, itr)
 
-
-def test_density2D():
+def test_density2D(new, itr=10):
     print("# 2D DensityCompensation")
-    img2d = get_sample_data("2d-pmri").data.astype(np.complex64)
     samples2d = get_sample_data("mri-radial-samples").data
-    samples2d *= 2 * np.pi
-    shape2d = (512, 512)
+    shape2d = (512,512)
     print("samples: ", samples2d.shape)
-    print("img size: ", img2d.shape)
-
-    grid_op = NonCartesianFFT(
-        samples=samples2d,
-        shape=shape2d,
-        implementation='gpuNUFFT',
-        osf=1,
-    )
-
-    density_new = grid_op.impl.operator.estimate_density_comp(10)
-    density_comp2d = estimate_density_compensation(samples2d, shape2d, 10)
-    print(np.allclose(density_comp2d, density_new))
-    print(np.linalg.norm(density_comp2d - density_new) / np.linalg.norm(density_comp2d))
-
+    print("img size ", shape2d)
+    return test_density(samples2d, shape2d, new, itr)
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.dim == 3:
-        test_density3D()
+        test_density3D(args.new, itr=args.iter)
     elif args.dim == 2:
-        test_density2D()
+        test_density2D(args.new, itr=args.iter)
     else:
-        test_density2D()
-        test_density3D()
+        test_density2D(args.new, itr=args.iter)
+        test_density3D(args.new, itr=args.iter)
