52143949c76a9ae8ef6a8dfe5aa33e3267796b3d
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-01-31 09:54:56 +0100

    add power method estimation.
    There is sadly no significant improvement vs the python version.

--- tests/test_power_method.py
+++ tests/test_power_method.py
@@ -9,7 +9,11 @@ from pysap.data import get_sample_data
 from modopt.math.matrix import PowerMethod
 
 
-def test_powermethod(samples, shape, n_coils, smaps, ):
+parser = argparse.ArgumentParser(description='test density_compensation.')
+parser.add_argument('dim', metavar='dim', type=int, const=0, default=0, nargs='?',
+                    help='select dimension')
+
+def test_powermethod(samples, shape, n_coils, smaps, density):
     f = NonCartesianFFT(samples=samples,
                         shape=shape,
                         density_comp=density,
@@ -19,19 +23,50 @@ def test_powermethod(samples, shape, n_coils, smaps, ):
                         )
     power = PowerMethod(lambda x: f.adj_op(f.op(x)),
                         data_shape=shape,
-                        data_type=np.complex64,
+                        data_type="complex64",
                         auto_run=False,
                         verbose=True)
-
     ts = perf_counter()
-    rad = power.get_spec_rad(extra_factor=1, max_iter=10)
+    power.get_spec_rad(extra_factor=1.0, max_iter=10)
+    rad = power.spec_rad
     tf = perf_counter()
     print("elapsed_time: {:.3f}s".format(tf-ts))
-    print("rad", r)
+    print("rad: ", rad)
 
     ts2 = perf_counter()
-    r = f.impl.operator.get_spectral_radius(max_iter=10)
+    rad_new = f.impl.operator.get_spectral_radius(10, 1e-6)
     tf2 = perf_counter()
     print("elapsed_time: {:.3f}s, x{:.1f}".format(
         tf2-ts2, (tf-ts)/(tf2-ts2)))
-    print("rad_new", np.linalg.norm(gradient_new.flatten()))
+    print("rad_new", rad_new)
+
+if __name__ == "__main__":
+
+    args = parser.parse_args()
+    if args.dim == 2:
+        img2d = get_sample_data("2d-pmri").data.astype("complex64")
+        samples2d = get_sample_data("mri-radial-samples").data
+        print(img2d.shape)
+        samples2d *= np.pi / samples2d.max()
+        shape2d = (512, 512)
+        n_coils = 32
+        smaps2d = np.ones_like(img2d, dtype='complex64')
+        smaps2d = smaps2d * np.arange(1, len(smaps2d) + 1)[:, np.newaxis, np.newaxis]
+        fake_data2d = np.ones(len(samples2d), dtype='complex64')
+        img2dssos = np.linalg.norm(img2d,axis=0)
+        density = np.ones(len(samples2d), dtype='float32')
+
+        test_powermethod(samples2d, shape2d, n_coils, smaps2d, density)
+    if args.dim == 3:
+        img3d = get_sample_data("3d-pmri").data.astype(np.complex64)
+        samples3d = get_sample_data("mri-radial-3d-samples").data
+        samples3d *= np.pi / samples3d.max()
+        n_coils = 32
+        shape3d = (128, 128, 160)
+        smaps3d = np.ones_like(img3d, dtype='complex128')
+        smaps3d = smaps3d * np.arange(1, len(smaps3d) + 1)[:, np.newaxis, np.newaxis, np.newaxis]
+        fake_data3d = np.ones(len(samples3d), dtype='complex64')
+        img3dssos = np.linalg.norm(img3d, axis=0)
+        density = np.ones(len(samples3d), dtype='float32')
+
+        test_powermethod(samples3d, shape3d, n_coils, smaps3d, density)
